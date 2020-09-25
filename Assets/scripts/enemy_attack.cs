562d404924aa4907d4a963dab3ebb3a7211cddde
juandev <juandiemore@gmail.com>
2020-09-25 09:15:11 -0500

    ultimos cambios

--- Assets/scripts/enemy_attack.cs
+++ Assets/scripts/enemy_attack.cs
@@ -6,11 +6,20 @@ public class enemy_attack : MonoBehaviour
 {
     // Start is called before the first frame update
 
+
+
 private void OnTriggerEnter(Collider other) {
+
+    // si el personaje hace un ataque frente a la capsula 
     if(other.gameObject.tag == "punch"){
+        // se destruye 
         Destroy(this.gameObject);
         Debug.Log("Buum");
+
+
     }else  if(other.gameObject.tag == "Player"){
+        // si la capsula toca al perosnaje 
+        // aparece este mensaje
         Debug.Log("aaah");
     }
 }
--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class move : MonoBehaviour
 {
 
@@ -44,12 +46,13 @@ public class move : MonoBehaviour
 
 // obtenemos la tecla de ataque
         if(Input.GetKeyDown("space")){
-
+// se dispara la animación y se activa el collider punch
             anim.SetTrigger("punch");
             punch.SetActive(true);
         }
 
         if(Input.GetKeyUp("space")){
+            // al soltar se apaga el collider
               
                punch.SetActive(false);
         }
@@ -57,31 +60,49 @@ public class move : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        // se aplica el movimiento
+       
+       // en pocas palabras se aplica un valor condo se preciona A o D (o las flechas de Izq y der)
        rotacion = Input.GetAxis("Horizontal")*velocidadDeRotacion;
         
-
+        // en pocas palabras se aplica un valor condo se preciona W o S (o las flechas de arriba y abajo)
         movimiento = Input.GetAxis("Vertical")*speed;
+
+        //  se multiplica por el tiempo en el que la tecla se presiona (llega a un valor maximo)
         movimiento *= Time.deltaTime;
         rotacion *= Time.deltaTime;
+        
+        // y se aplica el movimiento 
         transform.Translate(0,0,movimiento);
         transform.Rotate(0,rotacion,0);
 
+        // usamos una variable auxiliar para calcular cuando se ejecuta la animación
         aux = (int) ((movimiento*4) * speed);
 
+        // y se le pasa el valor a la animacion 
         anim.SetInteger("speed",aux);
 
 
-    
+
+        // contador cuando esta parado 
+
+        // fijamos el valor del contador = al tiempo actual (es necesario pasarlo de float a string)
         auxText= time.ToString();
         timeText.text = auxText;
-        if(aux == 0){
-            
-            timeText.enabled = true;
-            
 
+        if(aux == 0){
+            // si el pj esta quieto la el aux es igual a 0 
+            // se activa el contador en pantalla 
+            timeText.enabled = true;
+
+            // y se resta el tiempo
             time -= Time.deltaTime;
+
+            if(time <= 0){
+                // si se acaba el tiempo se carga la pantalla gameover
+                SceneManager.LoadScene("gameover");
+            }
         }else {
+            // si el personaje se mueve el contador para y se resetea 
             time = maxTime;
             timeText.text = maxTime.ToString();
             timeText.enabled = false;
